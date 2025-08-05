27cee9a4b060aab77930653aae22ac1f09979d9c
Enchanted-Games <isaacoblackshaw@gmail.com>
2025-08-05 02:04:01 +0100

    update to 1.21.4

--- src/main/java/io/github/burritobandit28/redstone_capacitor/RedstoneCapacitor.java
+++ src/main/java/io/github/burritobandit28/redstone_capacitor/RedstoneCapacitor.java
@@ -11,6 +11,8 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,19 +22,18 @@ public class RedstoneCapacitor implements ModInitializer {
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	private static final Identifier CAPACITOR_ID = Identifier.of(MOD_ID, "capacitor");
 	public static final RedstoneCapacitorBlock CAPACITOR = new RedstoneCapacitorBlock(
-			AbstractBlock.Settings.copy(Blocks.REPEATER)
+			AbstractBlock.Settings.copy(Blocks.REPEATER).registryKey(RegistryKey.of(RegistryKeys.BLOCK, CAPACITOR_ID))
 	);
-
-	public static final BlockItem CAPACITOR_BLOCK_ITEM = new BlockItem(CAPACITOR, new Item.Settings());
+	public static final BlockItem CAPACITOR_BLOCK_ITEM = new BlockItem(CAPACITOR,
+		new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, CAPACITOR_ID)).useBlockPrefixedTranslationKey()
+	);
 
 	@Override
 	public void onInitialize() {
-
-
-		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "capacitor"), CAPACITOR_BLOCK_ITEM);
-		Registry.register(Registries.BLOCK, Identifier.of(MOD_ID, "capacitor"), CAPACITOR);
-
+		Registry.register(Registries.ITEM, CAPACITOR_ID, CAPACITOR_BLOCK_ITEM);
+		Registry.register(Registries.BLOCK, CAPACITOR_ID, CAPACITOR);
 
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.REDSTONE).register(itemGroup -> {
 			itemGroup.add(CAPACITOR_BLOCK_ITEM);
